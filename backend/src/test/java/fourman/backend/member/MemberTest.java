5718ad6fcd0c05d68da4fe5a8f3b507cee541b98
MDandersonm <1215kimyj@gmail.com>
2023-03-23 20:37:55 +0900

    test: 로그인시 토큰과 함께 유저명이 담긴 response객체 반환 테스트 [CAFE-RESERVE-BACK-22]

--- backend/src/test/java/fourman/backend/member/MemberTest.java
+++ backend/src/test/java/fourman/backend/member/MemberTest.java
@@ -4,6 +4,7 @@ import fourman.backend.domain.member.entity.AuthorityType;
 import fourman.backend.domain.member.entity.ManagerCode;
 import fourman.backend.domain.member.repository.ManagerCodeRepository;
 import fourman.backend.domain.member.service.MemberService;
+import fourman.backend.domain.member.service.request.MemberLoginRequest;
 import fourman.backend.domain.member.service.request.MemberRegisterRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,5 +38,15 @@ public class MemberTest {
                 "manager@manager.com", "manager", "박관리", 19931106, AuthorityType.MANAGER,true,
                 "수원특별시","종로구","명동","어딘가","010-0000-0000");
         memberService.signUp(registerRequest);
+    }
+
+    @Test
+    void memberSignInTest() {
+        MemberRegisterRequest registerRequest = new MemberRegisterRequest(
+                "meme@me.com", "meme", "김영진", 19931106, AuthorityType.MEMBER, false,
+                "서울특별시","중랑구","면목동","어딘가","010-0000-0000");
+        memberService.signUp(registerRequest);
+        MemberLoginRequest loginRequest = new MemberLoginRequest("meme@me.com", "meme");
+        memberService.signIn(loginRequest);
     }
 }
