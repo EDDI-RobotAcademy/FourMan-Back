1792986284a727616775890943a8ff1dcb35aecf
Cong0327 <hsp400@gmail.com>
2023-03-23 20:24:17 +0900

    test:[박현수]  질문 게시판에 게시물 클릭 시 게시물을 읽을 수 있는 시스템 [CAFE-RESERVE-BACK-20]

--- backend/src/test/java/fourman/backend/questionBoard/QuestionBoardTest.java
+++ backend/src/test/java/fourman/backend/questionBoard/QuestionBoardTest.java
@@ -1,6 +1,7 @@
 package fourman.backend.questionBoard;
 
 import fourman.backend.domain.questionboard.controller.requestForm.QuestionBoardRequestForm;
+import fourman.backend.domain.questionboard.entity.QuestionBoard;
 import fourman.backend.domain.questionboard.service.QuestionBoardService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,5 +18,11 @@ public class QuestionBoardTest {
                 new QuestionBoardRequestForm("1", "2", "3","4");
 
         questionBoardService.register(questionBoardRequestForm);
+    }
+
+    @Test
+    public void readTest() {
+      QuestionBoard questionBoard = questionBoardService.read(1L);
+        System.out.println(questionBoard);
     }
 }
