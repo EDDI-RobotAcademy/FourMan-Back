606fde94886131531dd67d5f987b74da6bead1bc
MDandersonm <125858342+MDandersonm@users.noreply.github.com>
2023-03-28 04:25:59 +0900

    feat: [김영진] 카페소개버튼을 눌렀을때 카페리스트들이 나올 수있도록 카페정보 반환: [CAFE-RESERVE-BACK-31]

--- backend/src/main/java/fourman/backend/domain/cafeIntroduce/controller/CafeIntroduceController.java
+++ backend/src/main/java/fourman/backend/domain/cafeIntroduce/controller/CafeIntroduceController.java
@@ -2,6 +2,7 @@ package fourman.backend.domain.cafeIntroduce.controller;
 
 import fourman.backend.domain.cafeIntroduce.controller.requestForm.CafeIntroRequestForm;
 import fourman.backend.domain.cafeIntroduce.service.CafeIntroduceService;
+import fourman.backend.domain.cafeIntroduce.service.response.CafeIntroListResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -19,7 +20,7 @@ public class CafeIntroduceController {
 
     @PostMapping(value = "/register",
             consumes = {  MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE }) // 이미지+텍스트 업로드하는 경우 value , consumes 정보(이미지타입, json타입) 추가
-    public String registerProduct(
+    public String registerCafe(
             @RequestPart(value = "thumbnail") List<MultipartFile> thumbnail,
             @RequestPart(value = "fileList") List<MultipartFile> fileList,
             @RequestPart(value = "info") CafeIntroRequestForm cafeIntroRequestForm) {
@@ -31,4 +32,10 @@ public class CafeIntroduceController {
 
         return "카페가 등록되었습니다.";
     }
+
+    @GetMapping(path = "/list")
+    public List<CafeIntroListResponse> cafeList() {
+            return cafeIntroduceService.list();
+        }
+
 }
