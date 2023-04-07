11fd733281937c1164760b67ee3b6cc7b86a5ec4
Cong0327 <hsp400@gmail.com>
2023-04-07 17:19:24 +0900

    test:[박현수] feat:[박현수] 공지사항 게시판 수정하는 시스템 [CAFE-RESERVE-BACK-51]

--- backend/src/test/java/fourman/backend/noticeBoard/NoticeBoardTest.java
+++ backend/src/test/java/fourman/backend/noticeBoard/NoticeBoardTest.java
@@ -4,6 +4,8 @@ package fourman.backend.noticeBoard;
 import fourman.backend.domain.noticeBoard.controller.requestForm.NoticeBoardRequestForm;
 import fourman.backend.domain.noticeBoard.entity.NoticeBoard;
 import fourman.backend.domain.noticeBoard.service.NoticeBoardService;
+import fourman.backend.domain.questionboard.controller.requestForm.QuestionBoardRequestForm;
+import fourman.backend.domain.questionboard.entity.QuestionBoard;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,6 +25,14 @@ public class NoticeBoardTest {
     @Test
     public void readTest() {
         NoticeBoard noticeBoard = noticeBoardService.read(1L);
+        System.out.println(noticeBoard);
+    }
+
+    @Test
+    public void modifyTest() {
+        NoticeBoard noticeBoard = noticeBoardService.modify(3L,
+                new NoticeBoardRequestForm("고오옹지","공지사항","고오오오옹지1","공지111111",1L));
+
         System.out.println(noticeBoard);
     }
 }
