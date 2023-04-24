47208ee058fd542da67c34f6b8893b094e362c5e
Hyeonvv <pphhww5017@gmail.com>
2023-04-24 20:33:28 +0900

    feat: [박현우] 일반 회원이 마이페이지의 예약 내역 탭에서 확인할 수 있는 예약 좌석 정보와, 상품 주문 정보 반환 [CAFE-RESERVE-BACK-88]

--- src/main/java/fourman/backend/domain/order/repository/OrderReservationRepository.java
+++ src/main/java/fourman/backend/domain/order/repository/OrderReservationRepository.java
@@ -1,7 +1,15 @@
 package fourman.backend.domain.order.repository;
 
+import fourman.backend.domain.order.entity.OrderInfo;
 import fourman.backend.domain.order.entity.OrderReservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface OrderReservationRepository extends JpaRepository<OrderReservation, Long> {
+
+    @Query("select orr from OrderReservation orr join fetch orr.seatNoList where orr.id = :id")
+    Optional<OrderReservation> findByReservationId(Long id);
+
 }
