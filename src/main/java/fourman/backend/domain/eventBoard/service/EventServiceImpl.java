6989eb8a97fdae858acb1dc872dc28517c624add
jmj30040 <jmj30040@gmail.com>
2023-04-25 15:50:55 +0900

    Merge branch 'main' of https://github.com/jmj30040/FourMan-Back

--- src/main/java/fourman/backend/domain/eventBoard/service/EventServiceImpl.java
+++ src/main/java/fourman/backend/domain/eventBoard/service/EventServiceImpl.java
@@ -9,6 +9,7 @@ import fourman.backend.domain.member.entity.CafeCode;
 import fourman.backend.domain.member.repository.CafeCodeRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -57,7 +58,7 @@ public class EventServiceImpl implements EventService {
                 String thumbnailReName = 't'+thumbnailRandomName + multipartFile.getOriginalFilename();
 
                 //저장 경로 지정 + 파일네임
-                FileOutputStream writer1 = new FileOutputStream("../FourMan-Front/src/assets/event/uploadImgs/" + thumbnailReName);
+                FileOutputStream writer1 = new FileOutputStream("../FourMan-Front/public/assets/event/uploadImgs/" + thumbnailReName);
                 log.info("디렉토리에 파일 배치 성공!");
 
                 //파일 저장(저장할때는 byte 형식으로 저장해야 해서 파라미터로 받은 multipartFile 파일들의 getBytes() 메소드 적용해서 저장하는 듯)
@@ -139,6 +140,61 @@ public class EventServiceImpl implements EventService {
             return maybeEvent.get().getCafeCode().getCafe();
         }
         return null;
+    }
+
+    @Override
+    public Long modifyEvent(Long eventId, List<MultipartFile> thumbnail, EventRequestForm eventRequestForm) {
+        Optional<Event> optionalEvent = eventRepository.findById(eventId);
+        if (!optionalEvent.isPresent()) {
+            log.info("이벤트가존재하지 않습니다.");
+            return null;
+        }
+        Event event = optionalEvent.get();
+
+        // 1. event 업데이트
+        event.setEventName(eventRequestForm.getEventName());
+        event.setEventStartDate(eventRequestForm.getEventStartDate());
+        event.setEventEndDate(eventRequestForm.getEventEndDate());
+        event.setContent(eventRequestForm.getContent());
+        Optional<CafeCode> op= cafeCodeRepository.findByCode(eventRequestForm.getCode());
+        event.setCafeCode(op.get());
+
+        LocalDateTime now = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+
+        // 실제 파일 frontend 이미지 폴더 경로에 저장
+        try {
+            // 썸네일 저장
+            if (thumbnail != null && !thumbnail.isEmpty()) {
+                MultipartFile multipartFile = thumbnail.get(0);
+                log.info("modifyEvent() - Make file: " + multipartFile.getOriginalFilename());
+                String thumbnailRandomName = now.format(dtf);
+                String thumbnailReName = 't' + thumbnailRandomName + multipartFile.getOriginalFilename();
+
+                // 저장 경로 지정 + 파일네임
+                FileOutputStream writer1 = new FileOutputStream("../FourMan-Front/public/assets/event/uploadImgs/" + thumbnailReName);
+                log.info("디렉토리에 파일 배치 성공!");
+
+                // 파일 저장
+                writer1.write(multipartFile.getBytes());
+                event.setThumbnailFileName(thumbnailReName);
+                writer1.close();
+            }
+
+            // 수정된 이벤트 저장
+            eventRepository.save(event);
+            return event.getEventId();
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void deleteEvent(Long eventId) {
+        eventRepository.deleteById(eventId);
     }
 
 
